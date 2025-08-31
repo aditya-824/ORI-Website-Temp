d1f4784f0bca72a198c89e95dd2c528b8134476e
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-08-31 18:52:19 +0530

    Updates

--- src/components/timelineData.js
+++ src/components/timelineData.js
@@ -1,47 +1,38 @@
-import b150team from '../assets/cars/b150-team.jpg';
-import b600team from '../assets/cars/b600-team.jpg';
-import award2010 from '../assets/about-us/2010-award.jpg';
-import endurance2012 from '../assets/about-us/2012-endurance.png';
-import monocoque2018 from '../assets/about-us/2018-monocoque.jpg';
-import fb2018 from '../assets/about-us/fb-2018.jpg';
-import ev2019 from '../assets/about-us/2019-ev.jpg';
-import endurance2024 from '../assets/about-us/2024-endurance.jpg';
-
 export default [
     {
         year: 2006,
         event: "Team founded",
-        image: b150team
+        image: "/assets/cars/b150-team.jpg"
     },
     {
         year: 2007,
         event: "First international competition; Formula Student Germany",
-        image: b600team
+        image: "/assets/cars/b600-team.jpg"
     },
     {
         year: 2010,
         event: "First award won",
-        image: award2010
+        image: "/assets/about-us/2010-award.jpg"
     },
     {
         year: 2012,
         event: "Completed first-ever endurance event",
-        image: endurance2012
+        image: "/assets/about-us/2012-endurance.png"
     },
     {
         year: 2018,
         event: "First monocoque chassis built",
-        image: monocoque2018
+        image: "/assets/about-us/2018-monocoque.jpg"
     },
     {
         year: 2018,
         event: "Placed first at Formula Bharat",
-        image: fb2018
+        image: "/assets/about-us/fb-2018.jpg"
     },
     {
         year: 2019,
         event: "Shift from combustion powertrain to electric powertrain",
-        image: ev2019
+        image: "/assets/about-us/2019-ev.jpg"
     },
     {
         year: 2021,
@@ -51,6 +42,6 @@ export default [
     {
         year: 2024,
         event: "First endurance event completed with electric powertrain",
-        image: endurance2024
+        image: "/assets/about-us/2024-endurance.jpg"
     },
 ];
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,20 +1,18 @@
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Timeline from "../components/Timeline";
 
-import bella from "../assets/about-us/bellatrix-on-court.jpg"
-import fb2024 from "../assets/about-us/fb-2024.jpg";
-import pegasusTwo from "../assets/pegasus2.0.jpg";
+// import bella from "../assets/about-us/bellatrix-on-court.jpg"
+// import fb2024 from "../assets/about-us/fb-2024.jpg";
+// import pegasusTwo from "../assets/pegasus2.0.jpg";
 import timelineData from "../components/timelineData.js";
 
 function AboutUs() {
     return (
         <>
             <main>
-                <Image src={bella} alt="Bellatrix Prototype on basketball court" fluid />
+                <Image src="/assets/about-us/bellatrix-on-court.jpg" alt="Bellatrix Prototype on basketball court" fluid />
                 <Container className="about">
                     <h1>About Us</h1>
                     <p>As we’ve heard in a famous Hindi Film, "Don’t study to be successful, study to be able. Success will have to follow." This is exactly what happened in 2006, when an ambitious bunch of engineering students from our college, set out on this ambitious journey, to defy all odds, break the shackles of this monotonous education system, and make themselves able to compete at the international level. With the right tools in their hands, fire in their hearts, and with a battered rickshaw engine in a cramped workshop, they began their search for success. And the rest is history.</p>
