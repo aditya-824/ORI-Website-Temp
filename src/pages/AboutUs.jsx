a287f4d9c84c119b4f9c28fc292b819b110b6030
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-08-31 18:54:27 +0530

    Updates

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -12,7 +12,7 @@ function AboutUs() {
     return (
         <>
             <main>
-                <Image src="/assets/about-us/bellatrix-on-court.jpg" alt="Bellatrix Prototype on basketball court" fluid />
+                <Image src="/about-us/bellatrix-on-court.jpg" alt="Bellatrix Prototype on basketball court" fluid />
                 <Container className="about">
                     <h1>About Us</h1>
                     <p>As we’ve heard in a famous Hindi Film, "Don’t study to be successful, study to be able. Success will have to follow." This is exactly what happened in 2006, when an ambitious bunch of engineering students from our college, set out on this ambitious journey, to defy all odds, break the shackles of this monotonous education system, and make themselves able to compete at the international level. With the right tools in their hands, fire in their hearts, and with a battered rickshaw engine in a cramped workshop, they began their search for success. And the rest is history.</p>
