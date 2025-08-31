716dfbe745d80d1de47d50baff8a7a298da681c6
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-08-31 16:39:36 +0530

    Updates

--- src/components/cars.js
+++ src/components/cars.js
@@ -1,98 +1,131 @@
+import pegasus2 from '../assets/cars/pegasus-2.jpg';
+import pegasus2team from '../assets/cars/pegasus-2-team.jpg';
+import pegasus from '../assets/cars/pegasus.jpg';
+import pegasusteam from '../assets/cars/pegasus-team.jpg';
+import lemnos from '../assets/cars/lemnos.jpg';
+import lemnosteam from '../assets/cars/lemnos-team.jpg';
+import artemis from '../assets/cars/artemis.jpg';
+import artemisteam from '../assets/cars/artemis-team.jpg';
+import bellatrix from '../assets/cars/bellatrix.png';
+import bellatrixteam from '../assets/cars/bellatrix-team.jpg';
+import ori17c from '../assets/cars/ori-17c.jpg';
+import ori17cteam from '../assets/cars/ori-17c-team.jpg';
+import rigel from '../assets/cars/rigel.jpg';
+import rigelteam from '../assets/cars/rigel-team.jpg';
+import ori2014 from '../assets/cars/ori-2014.jpg';
+import ori2014team from '../assets/cars/ori-2014-team.jpg';
+import arion2 from '../assets/cars/arion-2.jpg';
+import arion2team from '../assets/cars/arion-2-team.jpg';
+import arion from '../assets/cars/arion.jpg';
+import arionteam from '../assets/cars/arion-team.jpg';
+import aeton from '../assets/cars/aeton.jpg';
+import aetonteam from '../assets/cars/aeton-team.jpg';
+import ori2010 from '../assets/cars/ori-2010.jpg';
+import ori2010team from '../assets/cars/ori-2010-team.jpg';
+import stallion from '../assets/cars/stallion.jpg';
+import stallionteam from '../assets/cars/stallion-team.jpg';
+import itana from '../assets/cars/itana.jpg';
+import itanateam from '../assets/cars/itana-team.jpg';
+import b600 from '../assets/cars/b600.jpg';
+import b600team from '../assets/cars/b600-team.jpg';
+import b150 from '../assets/cars/b150.jpg';
+import b150team from '../assets/cars/b150-team.jpg';
+
 export default [
     {
-        image: "/src/assets/cars/pegasus-2.jpg",
+        image: pegasus2,
         name: "Pegasus 2.0",
         year: 2024,
-        team: "/src/assets/cars/pegasus-2-team.jpg",
+        team: pegasus2team,
     },
     {
-        image: "/src/assets/cars/pegasus.jpg",
+        image: pegasus,
         name: "Pegasus",
         year: 2023,
-        team: "/src/assets/cars/pegasus-team.jpg",
+        team: pegasusteam,
     },
     {
-        image: "/src/assets/cars/lemnos.jpg",
+        image: lemnos,
         name: "Lemnos",
         year: 2022,
-        team: "/src/assets/cars/lemnos-team.jpg",
+        team: lemnosteam,
     },
     {
-        image: "/src/assets/cars/artemis.jpg",
+        image: artemis,
         name: "Artemis",
         year: 2019,
-        team: "/src/assets/cars/artemis-team.jpg",
+        team: artemisteam,
     },
     {
-        image: "/src/assets/cars/bellatrix.png",
+        image: bellatrix,
         name: "Bellatrix",
         year: 2018,
-        team: "/src/assets/cars/bellatrix-team.jpg",
+        team: bellatrixteam,
     },
     {
-        image: "/src/assets/cars/ori-17c.jpg",
+        image: ori17c,
         name: "ORI 17C",
         year: 2017,
-        team: "/src/assets/cars/ori-17c-team.jpg",
+        team: ori17cteam,
     },
     {
-        image: "/src/assets/cars/rigel.jpg",
+        image: rigel,
         name: "Rigel",
         year: 2016,
-        team: "/src/assets/cars/rigel-team.jpg",
+        team: rigelteam,
     },
     {
-        image: "/src/assets/cars/ori-2014.jpg",
+        image: ori2014,
         name: "ORI 2014",
         year: 2014,
-        team: "/src/assets/cars/ori-2014-team.jpg",
+        team: ori2014team,
     },
     {
-        image: "/src/assets/cars/arion-2.jpg",
+        image: arion2,
         name: "Arion 2.0",
         year: 2013,
-        team: "/src/assets/cars/arion-2-team.jpg",
+        team: arion2team,
     },
     {
-        image: "/src/assets/cars/arion.jpg",
+        image: arion,
         name: "Arion",
         year: 2012,
-        team: "/src/assets/cars/arion-team.jpg",
+        team: arionteam,
     },
     {
-        image: "/src/assets/cars/aeton.jpg",
+        image: aeton,
         name: "Aeton",
         year: 2011,
-        team: "/src/assets/cars/aeton-team.jpg",
+        team: aetonteam,
     },
     {
-        image: "/src/assets/cars/ori-2010.jpg",
+        image: ori2010,
         name: "ORI 2010",
         year: 2010,
-        team: "/src/assets/cars/ori-2010-team.jpg",
+        team: ori2010team,
     },
     {
-        image: "/src/assets/cars/stallion.jpg",
+        image: stallion,
         name: "Stallion",
         year: 2009,
-        team: "/src/assets/cars/stallion-team.jpg",
+        team: stallionteam,
     },
     {
-        image: "/src/assets/cars/itana.jpg",
+        image: itana,
         name: "Itana",
         year: 2008,
-        team: "/src/assets/cars/itana-team.jpg",
+        team: itanateam,
     },
     {
-        image: "/src/assets/cars/b600.jpg",
+        image: b600,
         name: "B600",
         year: 2007,
-        team: "/src/assets/cars/b600-team.jpg",
+        team: b600team,
     },
     {
-        image: "/src/assets/cars/b150.jpg",
+        image: b150,
         name: "B150",
         year: 2006,
-        team: "/src/assets/cars/b150-team.jpg",
+        team: b150team,
     },
 ]
