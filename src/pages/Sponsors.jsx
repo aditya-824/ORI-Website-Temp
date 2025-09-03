3d4e797129b8bea89b52a0866d6b932ce076e36d
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-09-03 23:00:45 +0530

    Fixed sponsors page

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -2,7 +2,7 @@ import Footer from "../components/Footer";
 import Container from "react-bootstrap/Container";
 import SponsorsGrid from "../components/SponsorsGrid";
 
-import sponsors from "../components/sponsors.js";
+import sponsorsData from "../components/sponsorsData.js";
 
 function Sponsors() {
     return (
@@ -12,7 +12,7 @@ function Sponsors() {
                     <h1>Our Sponsors</h1>
                     <p>We at Orion Racing India strive day and night to break all the barriers and continuously think of new innovative methods to shape the automobile industry. Participating in competitions like Formula Student Germany and Spain helps us to work harder and dream bigger. Your company can become a part of our incredible journey, and you can share the pride of supporting and molding a team representing the country on a global scale.</p>
                 </Container>
-                <SponsorsGrid sponsors={sponsors} />
+                <SponsorsGrid sponsors={sponsorsData} />
             </main>
             <Footer />
         </>
