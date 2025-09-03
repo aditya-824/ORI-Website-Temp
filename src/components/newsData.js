ed0832286e6e8d32b973b8d1b91e1f012789ee52
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-09-03 23:12:25 +0530

    Fixed timeline, media

--- src/components/newsData.js
+++ src/components/newsData.js
@@ -1,29 +1,55 @@
-const news =[
-    "src/assets/media/news1.jpg",
-    "src/assets/media/news2.jpg",
-    "src/assets/media/news3.jpg",
-    "src/assets/media/news4.jpg",
-    "src/assets/media/news5.jpg",
-    "src/assets/media/news6.jpg",
-    "src/assets/media/news7.jpg",
-    "src/assets/media/news8.jpg",
-    "src/assets/media/news9.jpg",
-    "src/assets/media/news10.jpg",
-    "src/assets/media/news11.jpg",
-    "src/assets/media/news12.jpg",
-    "src/assets/media/news13.jpg",
-    "src/assets/media/news14.jpg",
-    "src/assets/media/news15.jpg",
-    "src/assets/media/news16.jpg",
-    "src/assets/media/news17.jpg",
-    "src/assets/media/news18.jpg",
-    "src/assets/media/news19.jpg",
-    "src/assets/media/news20.jpg",
-    "src/assets/media/news21.jpg",
-    "src/assets/media/news22.jpg",
-    "src/assets/media/news23.jpg",
-    "src/assets/media/news24.jpg",
-    "src/assets/media/news25.jpg"
+import news1 from "../assets/media/news1.jpg";
+import news2 from "../assets/media/news2.jpg";
+import news3 from "../assets/media/news3.jpg";
+import news4 from "../assets/media/news4.jpg";
+import news5 from "../assets/media/news5.jpg";
+import news6 from "../assets/media/news6.jpg";
+import news7 from "../assets/media/news7.jpg";
+import news8 from "../assets/media/news8.jpg";
+import news9 from "../assets/media/news9.jpg";
+import news10 from "../assets/media/news10.jpg";
+import news11 from "../assets/media/news11.jpg";
+import news12 from "../assets/media/news12.jpg";
+import news13 from "../assets/media/news13.jpg";
+import news14 from "../assets/media/news14.jpg";
+import news15 from "../assets/media/news15.jpg";
+import news16 from "../assets/media/news16.jpg";
+import news17 from "../assets/media/news17.jpg";
+import news18 from "../assets/media/news18.jpg";
+import news19 from "../assets/media/news19.jpg";
+import news20 from "../assets/media/news20.jpg";
+import news21 from "../assets/media/news21.jpg";
+import news22 from "../assets/media/news22.jpg";
+import news23 from "../assets/media/news23.jpg";
+import news24 from "../assets/media/news24.jpg";
+import news25 from "../assets/media/news25.jpg";
+
+const news = [
+    news1,
+    news2,
+    news3,
+    news4,
+    news5,
+    news6,
+    news7,
+    news8,
+    news9,
+    news10,
+    news11,
+    news12,
+    news13,
+    news14,
+    news15,
+    news16,
+    news17,
+    news18,
+    news19,
+    news20,
+    news21,
+    news22,
+    news23,
+    news24,
+    news25
 ];
 
 export default news;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,8 @@
-import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Timeline from "../components/Timeline";
 
-// import bella from "../assets/about-us/bellatrix-on-court.jpg"
+import bella from "../assets/about-us/bellatrix-on-court.jpg"
 // import fb2024 from "../assets/about-us/fb-2024.jpg";
 // import pegasusTwo from "../assets/pegasus2.0.jpg";
 import timelineData from "../components/timelineData.js";
@@ -12,7 +11,7 @@ function AboutUs() {
     return (
         <>
             <main>
-                <Image src="/about-us/bellatrix-on-court.jpg" alt="Bellatrix Prototype on basketball court" fluid />
+                <Image src={ bella } alt="Bellatrix Prototype on basketball court" fluid />
                 <Container className="about">
                     <h1>About Us</h1>
                     <p>As we’ve heard in a famous Hindi Film, "Don’t study to be successful, study to be able. Success will have to follow." This is exactly what happened in 2006, when an ambitious bunch of engineering students from our college, set out on this ambitious journey, to defy all odds, break the shackles of this monotonous education system, and make themselves able to compete at the international level. With the right tools in their hands, fire in their hearts, and with a battered rickshaw engine in a cramped workshop, they began their search for success. And the rest is history.</p>
